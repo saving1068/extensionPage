b1f8120723f0ece2965397405390be6fac4c7a8e
saving1068 <951068485@qq.com>
2020-07-22 17:04:58 +0800

    commit

---
+++ src/views/Home.vue
@@ -0,0 +1,200 @@
+<template>
+  <div class="warp">
+			<div class="show">
+				
+			</div>
+			<div class="content">
+				<div class="text-area">
+					<div class="title">{{title}}</div>
+					<div class="swiperBox">
+						<van-swipe class="swiper" indicator-dots indicator-active-color='#fff' :autoplay='5000' loop :duration="500">
+							<van-swipe-item v-for="(item,index) in project" :key='index'>
+								<div class="swiperTitle">
+									{{item.name}}
+								</div>
+								<div class="swiper-item-image">
+									<van-image :src="item.url" :fit="item.mode"/>
+								</div>
+								<div class="swiperTitle">
+									{{item.introduce}}
+								</div>
+							</van-swipe-item>
+							
+						</van-swipe>
+					</div>
+					<div class="info">
+						<div class="code">
+							<van-image :src="code" fit="fill"/>
+						</div>
+						<div class="text">
+							<div>长按扫码 联系我</div>
+							<div>永久维护</div>
+							<div>联系方式 微信同号13826111068</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		
+	</div>
+</template>
+
+<script  lang="ts">
+// @ is an alias to /src
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import 'vant/lib/index.css';
+import { Swipe, SwipeItem,Image as VanImage} from 'vant';
+
+Vue.use(Swipe);
+Vue.use(SwipeItem);
+@Component({
+  components: { Swipe,SwipeItem,VanImage}
+})
+  export default class App extends Vue {
+
+    // 初始化数据
+  title='项目介绍';
+  project=[
+    {
+      url:require('@/image/gongxiangshizi.png'),
+      type:0,
+      requireUrl:"https://www.litakeji.com/#/home",
+      name:"共享师资官网",
+      introduce:'一个做可直播的授课平台',
+      mode:'fill'
+    },
+    {
+      url:require('@/image/jiaxin.png'),
+      type:0,
+      requireUrl:"http://crm.jiaxqiye.com",
+      name:"嘉信招商自动化系统",
+      introduce:'一个关于公司管理的平台',
+      mode:'fill'
+    },
+    {
+      url:require('@/image/myt.png'),
+      type:1,
+      requireUrl:"https://www.litakeji.com/#/home",
+      name:"搬金蚁小程序",
+      introduce:'',
+      mode:'fill'
+    }
+  ];
+  code=require('@/image/code.png');
+  msg = 123;
+
+    // 声明周期钩子
+  mounted () {
+      // this.greet()
+    }
+
+    // 计算属性
+    get computedMsg () {
+      return 'computed ' + this.msg
+    }
+
+  
+
+    // 方法
+  //  onClickLeft () {
+  //     Toast('返回');
+  //   }
+
+  //   onClickRight () {
+  //     Toast('按钮');
+  //   }
+
+  //   greet () {
+  //     Toast('返回');
+  //   }
+    
+  }
+
+</script>
+<style lang='scss'>
+img{
+		width: 100%;
+		height: 100%;
+	}
+	.warp{
+		/* position: absolute; */
+		font-size: 0.42rem;
+		}
+		.show{
+			width: 100%;
+			height: 9rem;
+			background: url(../image/pic.png) no-repeat;
+			background-size: 100% 100%;
+		}
+	.content {
+		display: flex;
+		/* flex-direction: column; */
+		justify-content: center;
+		background: #1C2630;
+		padding: 0 .25rem 2rem;	
+		/* position: relative; */
+		/* position: fixed; */
+		width: 100%;
+		/* height: 700rem; */
+		
+		box-sizing: border-box;
+	}
+
+	.text-area {
+		/* display: flex;
+		justify-content: center; */
+		background: #FFFFFF;
+		width: 100%;
+		border-radius:.2rem ;
+		padding: .25rem .25rem ; 
+		box-sizing: border-box;
+	}
+
+	.title {
+		font-size: 0.42rem;
+		color: #fff;
+		width: 3rem;
+		height: .85rem;
+		margin: 0 auto;
+		background: url(../image/title.png) no-repeat ;
+		background-size: 100% 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	.swiperBox{
+		height: 7rem;
+		width: 100%;
+		padding:.5rem 0 ;
+		box-sizing: border-box;
+		text-align: center;
+		color: #007AFF;
+		
+	}
+	.swiper{
+		height: 100%;
+	}
+	.swiper-item-image{
+		width: 100%;
+    padding:.2rem 0;
+		/* height: 650rem; */
+		
+	}
+	.info{
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		padding: .3rem;
+		box-sizing: border-box;
+		background: #508DF3;
+		border-radius: .2rem;
+		color: #fff;
+	}
+	.code{
+		width: 2rem;
+		height: 1.5rem;
+		}
+		.text{
+			padding-left: .30rem;
+		}
+</style> 
---
+++ vue.config.js
@@ -0,0 +1,215 @@
+// console.log(process.env.VUE_APP_BASE_URL)
+console.log(process.env.NODE_ENV)
+// console.log(process.env.VUE_APP_BASE_URL)
+
+// const path = require('path')
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const merge = require("webpack-merge");
+const tsImportPluginFactory = require("ts-import-plugin");
+const path = require('path')
+function resolve(dir) {
+ return path.join(__dirname, '.', dir)
+}
+// const productionGzipExtensions = ['js', 'css']
+module.exports = {
+	// 选项...
+	//例如，如果你的应用被部署在 https://www.my-app.com/my-app/，则设置 publicPath 为 /my-app/。
+	publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+	//当运行 vue-cli-service build 时生成的生产环境构建文件的目录。
+	outputDir: 'dist',
+	//放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录。
+	assetsDir: './',
+	//指定生成的 index.html 的输出路径 (相对于 outputDir)。也可以是一个绝对路径
+	indexPath: 'index.html',
+	//关闭文件名哈希
+	filenameHashing: false,
+	pages: {
+		index: {
+			// page 的入口
+			entry: 'src/main.ts',
+			// 模板来源
+			template: 'public/index.html',
+			// 在 dist/index.html 的输出
+			filename: 'index.html',
+			// 当使用 title 选项时，
+			// template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
+			title: '推广',
+			favicon:'public/favicon.ico',
+			// 在这个页面中包含的块，默认情况下会包含
+			// 提取出来的通用 chunk 和 vendor chunk。
+			chunks: ['chunk-vendors', 'chunk-common', 'index']
+		},
+	},
+
+	//让 lint 错误在开发时直接显示在浏览器中 true警告仅仅会被输出到命令行，且不会使得编译失败。
+	// lintOnSave: process.env.NODE_ENV !== 'production' ? 'error' : false,
+	lintOnSave: false,
+
+	runtimeCompiler: false,
+	//你想要通过 Babel 显式转译一个依赖，可以在这个选项中列出来。没被编译的依赖
+	transpileDependencies: [],
+	//是否看未加密的错误位置
+	productionSourceMap: true,
+
+	integrity: false,
+	configureWebpack: config => {
+		// config.externals = {
+		// 	'vue': 'Vue',
+        //     'vue-router': 'VueRouter',
+        //     'vuex': 'Vuex',
+        //     'axios': 'axios'
+
+		// }
+
+		if (process.env.NODE_ENV === 'production') {
+			// config.optimization.minimizer[0].options.terserOptions.compress.warnings = false
+			// config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true
+			// config.optimization.minimizer[0].options.terserOptions.compress.drop_debugger = true
+			// config.optimization.minimizer[0].options.terserOptions.compress.pure_funcs = ['console.log']
+
+			config.plugins.push(new CompressionWebpackPlugin({
+				algorithm: 'gzip',
+				test:/\.js$|\.html$|\.css/,
+				threshold: 10240,
+				deleteOriginalAssets: false,
+				minRatio: 0.8
+			  }))
+			//   config.plugins.push(new HtmlWebpackPlugin({ // 打包输出HTML
+				
+			// 	minify: { // 压缩HTML文件
+			// 	  removeComments: true, // 移除HTML中的注释
+			// 	  collapseWhitespace: true, // 删除空白符与换行符
+			// 	  minifyCSS: true,// 压缩内联css
+			// 	  minifyJS: true,
+			// 	  removeComments: true,
+			// 	  removeCommentsFromCDATA: true,
+			// 	},
+				
+			//   //   filename: 'index.html',
+			//   //   template: 'index.html'
+			//   }))
+
+
+				
+
+			// console.log(config)
+			// 为生产环境修改配置...
+		} else {
+			// process.env.BASE_API = '/api/'
+			// 为开发环境修改配置...
+		}
+	},
+	chainWebpack: config => {
+		config.module
+			.rule('vue')
+			.use('vue-loader')
+			.loader('vue-loader')
+			.tap(options => {
+				// 修改它的选项...
+				options = merge(options, {
+					transpileOnly: true,
+					getCustomTransformers: () => ({
+					  before: [
+						tsImportPluginFactory({
+						  libraryName: "vant",
+						  libraryDirectory: "es",
+						  style: true
+						})
+					  ]
+					}),
+					compilerOptions: {
+					module: "es2015"
+					}
+				});
+				return options
+			})
+			// .rule("ts")
+			// .use("ts-loader")
+			// .loader('ts-loader')
+			// .tap(options => {
+			// 	// 修改它的选项...
+			// 	options = merge(options, {
+			// 		transpileOnly: true,
+			// 		getCustomTransformers: () => ({
+			// 		  before: [
+			// 			tsImportPluginFactory({
+			// 			  libraryName: "vant",
+			// 			  libraryDirectory: "es",
+			// 			  style: true
+			// 			})
+			// 		  ]
+			// 		}),
+			// 		compilerOptions: {
+			// 		module: "es2015"
+			// 		}
+			// 	});
+			// 	return options
+			// })
+			
+			
+			// config.module.rules.delete("svg"); //重点:删除默认配置中处理svg,
+			// //const svgRule = config.module.rule('svg')
+			// //svgRule.uses.clear()
+			// config.module
+			//  .rule('svg-sprite-loader')
+			//  .test(/\.svg$/)
+			//  .include
+			//  .add(resolve('src/components/iconSvg')) //处理svg目录
+			//  .end()
+			//  .use('svg-sprite-loader')
+			//  .loader('svg-sprite-loader')
+			//  .options({
+			//   symbolId: 'icon-[name]'
+			//  })
+			
+		// svgRule
+		// 	.test(/\.svg$/)
+		// 	.include.add(path.resolve(__dirname, './src/icons'))
+		// 	.end()
+		// 	.use('svg-sprite-loader')
+		// 	.loader('svg-sprite-loader')
+		// 	.options({
+		// 		symbolId: 'icon-[name]'
+		// 	})
+		// const fileRule = config.module.rule('file')
+		// fileRule.uses.clear()
+		// fileRule
+		// 	.test(/\.svg$/)
+		// 	.exclude.add(path.resolve(__dirname, './src/icons'))
+		// 	.end()
+		// 	.use('file-loader')
+		// 	.loader('file-loader')
+
+
+	},
+	//设置为 true 后你就可以去掉文件名中的 .module 并将所有的 *.(css|scss|sass|less|styl(us)?) 文件视为 CSS 
+	// css: {
+	//     modules: true
+	//   },
+	devServer: {
+		proxy: {
+			'/api': {
+				target: 'http://wearewwx.com:8001',
+				// target: 'http://2821r97k36.wicp.vip',
+				// ws: true,
+				changeOrigin: true,
+				pathRewrite: {
+					'^/api': '/'
+				}
+			},
+		}
+	},
+	// pluginOptions:{
+	// 	pwa: {
+	// 		iconPaths: {
+	// 		  favicon32: './favicon.ico',
+	// 		  favicon16: './favicon.ico',
+	// 		  appleTouchIcon: './favicon.ico',
+	// 		  maskIcon: './favicon.ico',
+	// 		  msTileImage: './favicon.ico'
+	// 		}
+	// 	  }
+	// }
+	
+}
